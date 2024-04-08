642a8c3b3a0da46936157f22e2f7df82630eb47f
jdh <120021993+ixueglot@users.noreply.github.com>
2024-04-08 20:55:44 +0300

    Update 1488.cpp

--- 1488.cpp
+++ 1488.cpp
@@ -9,54 +9,62 @@ int main() {
 	std::ofstream out;
 	//создаю переменные
 	string resultST;
+	string input;
 	int long result;
 	double long printresult;
-	double long firstparam, ffirstparam, secondparam, msecondparam, smartparam, 
-		ssmartparam, count, step, CountClon, answer;
-	bool start = true;
-	int col, e, i {0};
+	double long firstparam[10], secondparam[10], smartparam[10],
+		count, step, CountClon, answer;
+	int col, e, i{ 0 };
 	double long eClon;
 	out.open("col.txt");
 	//спрашиваю у пользователя сколько повторений нужно и какой будет ответ
+	cout << "Start запустит код, set зайдет в настройки" << endl << ">>>";
+	cin >> input;
+	if (input == "set") {
+
+	}
 	cout << "Сколько повторений вам надо?" << endl << ">>>";
 	cin >> col;
 	cout << "Какой ответ вам нужен вам нужен?" << endl << ">>>";
 	out << col << " Повторений" << endl;
 	cin >> answer;
 	//создаю цикл
-	while (start)
+	while (!(i == col))
 	{
 		cout << setprecision(10);
 		//создаю рандомные числа
 		step = (rand() % 10 * 1);
 		count = (rand() % 10 * 1);
 		e = rand() % 10 + 1;
-		ffirstparam = (double)(rand()) / 200000 * (200000 - -20000) + -20000;
-		firstparam = (double)(rand()) / 200000 * (200000 - -20000) + -20000;
-		secondparam = (double)(rand()) / 200000 * (200000 - -20000) + -20000;
-		msecondparam = (double)(rand()) / 200000 * (200000 - -20000) + -20000;
-		smartparam = (double)(rand()) / 200000 * (200 - -200) + -200;
-		ssmartparam = (double)(rand()) / 200000 * (200 - -200) + -200;
+		firstparam[0] = (double)(rand()) / 200000 * (200000 - -20000) + -20000;
+		firstparam[1] = (double)(rand()) / 200000 * (200000 - -20000) + -20000;
+		secondparam[0] = (double)(rand()) / 200000 * (200000 - -20000) + -20000;
+		secondparam[1] = (double)(rand()) / 200000 * (200000 - -20000) + -20000;
+		smartparam[0] = (double)(rand()) / 200000 * (200 - -200) + -200;
+		smartparam[1] = (double)(rand()) / 200000 * (200 - -200) + -200;
 		CountClon = pow(count, step);
 		eClon = exp(e);
 		//считаю ответ
-		result = eClon + ssmartparam * msecondparam - smartparam * CountClon + secondparam + firstparam + ffirstparam;
+		result = eClon + smartparam[0] * secondparam[0] - smartparam[1] * CountClon + secondparam[1] + firstparam[0] + firstparam[1];
 		//cout << "e" << e << " + " << ssmartparam << " * " << msecondparam << " - " << smartparam << " * " << count << "^" << step << " + " << secondparam << " + " << firstparam << " + " << ffirstparam << " = " << result << endl;
 		//Если ответ будет равен тому что указал пользователь
-		if (result == (answer) && start) {		
-			printresult = eClon + ssmartparam * msecondparam - smartparam * CountClon + secondparam + firstparam + ffirstparam;
+		if (result == (answer)) {
+			printresult = eClon + smartparam[0] * secondparam[0] - smartparam[1] * CountClon + secondparam[1] + firstparam[0] + firstparam[1];
 			//вывожу в  терминал
-			cout << "e" << e << " + " << ssmartparam << " * " << msecondparam << " - " << smartparam << " * " << count << "^" << step << " + " << secondparam << " + " << firstparam << " + " << ffirstparam << " = " << printresult << endl;
+			cout << "e" << e << " + " << smartparam[0] << " * " << secondparam[0] << " - " << smartparam[1] << " * " <<
+				count << "^" << step << " + " << secondparam[1] << " + " << firstparam[0] << " + " << firstparam[1] << " = " <<
+				printresult << endl;
 			//сохраняю в файл
-			out << "e" << e << " + " << ssmartparam << " * " << msecondparam << " - " << smartparam << " * " << count << "^" << step << " + " << secondparam << " + " << firstparam << " + " << ffirstparam << " = " << printresult << endl;
+			out << "e" << e << " + " << smartparam[0] << " * " << secondparam[0] << " - " << smartparam[1] << " * " <<
+				count << "^" << step << " + " << secondparam[1] << " + " << firstparam[0] << " + " << firstparam[1] << " = " <<
+				printresult << endl;
 			i++;
 			if (i == col) {
 				//закрываю файл
 				out.close();
-				start = false;
 			}
 		}
-		else{
+		else {
 		}
 	}
 }
